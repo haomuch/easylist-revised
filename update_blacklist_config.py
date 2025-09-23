de5f48fabe10a38f321228dfc20d5bf49be338d3
Haomuch <Michael.liuhao@gmail.com>
2025-09-23 11:18:58 +0800

    Update update_blacklist_config.py

--- update_blacklist_config.py
+++ update_blacklist_config.py
@@ -105,8 +105,10 @@ DOMAIN-SUFFIX,omnycontent.com,PROXY
 DOMAIN-SUFFIX,mc.tritondigital.com,PROXY
 DOMAIN-SUFFIX,tracking.swap.fm,PROXY
 DOMAIN-SUFFIX,podtrac.com,PROXY
+DOMAIN-SUFFIX,files.oaiusercontent.com,PROXY
 DOMAIN-SUFFIX,dowjones.io,PROXY"""
 
     # Run the function
     update_config_file(config_url, rules_to_add)
+
 
