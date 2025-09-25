807bfa51efd13add11ce2e4c6ba9a908ea8f8a66
Haomuch <Michael.liuhao@gmail.com>
2025-09-25 08:46:51 +0800

    Update update_blacklist_config.py
    remove *.googlevideo.com

--- update_blacklist_config.py
+++ update_blacklist_config.py
@@ -28,6 +28,8 @@ def update_config_file(url, new_rules, output_filename='optimized_blacklist.conf
                 updated_lines.append(line)
             # Modify the MITM line
             if line.startswith("hostname ="):
+                line = line.replace(",*.googlevideo.com", "")
+            #remove *.googlevideo.com
                 if "yt3.ggpht.com" not in line:
                     line += ",yt3.ggpht.com,analytics.googleapis.com,ads.googleapis.com,*.doubleclick.net"
                 updated_lines.append(line)
@@ -116,6 +118,7 @@ DOMAIN-SUFFIX,dowjones.io,PROXY"""
 
     # Run the function
     update_config_file(config_url, rules_to_add)
+
 
 
 
